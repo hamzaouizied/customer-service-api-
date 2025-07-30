f35f6d47a3b14a200ce9732f7a9345215dbdf84f
Hamzaoui zied <hamzaoui.zied.93@gmail.com>
2025-07-30 21:25:35 +0100

    fix request Customer and Service

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
-    //
+    protected $fillable = ['customer_id', 'name', 'description'];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
 }
