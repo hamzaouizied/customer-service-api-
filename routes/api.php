e7249aacf8de62cf5fd4db42fb470188ba86c4f1
Hamzaoui zied <hamzaoui.zied.93@gmail.com>
2025-07-30 20:24:00 +0100

    update routes

--- routes/api.php
+++ routes/api.php
@@ -8,22 +8,20 @@ use App\Http\Controllers\Api\CustomerAuthController;
 
 Route::get('test', fn() => response()->json(['success' => true]));
 
-Route::post('register', [CustomerAuthController::class, 'register']);
-Route::post('login', [CustomerAuthController::class, 'login']);
+Route::post('register', [CustomerAuthController::class, 'register']);// register new customer
+Route::post('login', [CustomerAuthController::class, 'login']);//login customer and get token
 
 Route::prefix('customer')->group(function () {
     Route::middleware('auth:customer-api')->group(function () {
+        //customer Api
         Route::get('show/{customer}', [CustomerController::class, 'show']); //View customer
         Route::get('all', [CustomerController::class, 'getAll']);//View all customers
         Route::put('update/{customer}', [CustomerController::class, 'update']);//Update customer
         Route::delete('delete/{customer}', [CustomerController::class, 'delete']);//Delete customer
-
+        //Auth jwt with Customer model
         Route::post('logout', [CustomerAuthController::class, 'logout']);
         Route::post('refresh', [CustomerAuthController::class, 'refresh']);
-
-        Route::apiResource('customers', CustomerController::class);
-        Route::apiResource('services', ServiceController::class);
-
+        //Service Api
         Route::get('customers/{id}/services', [ServiceController::class, 'customerServices']);
     });
 });
